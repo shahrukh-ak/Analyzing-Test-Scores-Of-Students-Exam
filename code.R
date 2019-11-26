13b170bfe9be87bee0eb117554eed62e82b63400
shahrukh-ak <55116845+shahrukh-ak@users.noreply.github.com>
2019-11-26 13:22:40 -0800

    Adding code

---
+++ code.R
@@ -0,0 +1,16 @@
+test1=c(56,78,87,89,95,98, NA,78,87,98,54,89,78,98,97)
+test2=c(86,67,78,89,87,67,94,78,81,83,78, NA,93,98,100)
+
+#How many students have their test 1 score greater than 80?
+which(test1>80)
+[1]  3  4  5  6  9 10 12 14 15
+sum(which(test1>80))
+[1] 78
+length(which(test1>80))
+[1] 9
+
+#How many students have their test 2 score greater than 85 ?
+which(test2>85)
+[1]  1  4  5  7 13 14 15
+length(which(test2>85))
+[1] 7
