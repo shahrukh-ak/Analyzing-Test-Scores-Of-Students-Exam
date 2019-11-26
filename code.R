ad3a3897896991a30b4f9d065803127188ed41c1
shahrukh-ak <55116845+shahrukh-ak@users.noreply.github.com>
2019-11-26 13:30:51 -0800

    Modifying

--- code.R
+++ code.R
@@ -14,3 +14,30 @@ which(test2>85)
 [1]  1  4  5  7 13 14 15
 length(which(test2>85))
 [1] 7
+
+#Did all fifteen students take both tests?
+any(is.na(test1))
+[1] TRUE
+any(is.na(test2))
+[1] TRUE
+#NA available so the answer is "NO"
+
+#How many students did better in the second test than the first test?
+which(test2>test1)
+[1]  1 11 13 15
+length(which(test2>test1))
+[1] 4
+
+
+#How many students have the same score in the first and second test?
+which(test1==test2)
+[1]  4  8 14
+length(which(test1==test2))
+[1] 3
+
+
+
+
+
+
+
